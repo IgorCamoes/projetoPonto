31543d8c20eaa7669e9c58b32cf905e4122d5b10
Igor Camões <itscamoes@gmail.com>
2019-05-29 16:33:46 -0300

    Arrumar script e criar tela de cadastro

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,2 +1,11 @@
-let spanFechar = document.querySelector(".fechaModal");
+let spanFechar = document.querySelector('.fechaModal');
 let menuModal = document.querySelector('.modalMenu');
+let confMsg = document.querySelector('.modalConteudo > p:first-child')
+
+function fechar(){
+    menuModal.style.display = 'none';
+}
+
+function 
+
+spanFechar.onclick = fechar;
